c7b50012a16aa3d9e36aaef7db74c54f057468cb
javipzv <90279135+javipzv@users.noreply.github.com>
2024-10-05 22:05:30 +0200

    some changes to dijkstra algorithm

--- dijkstra.py
+++ dijkstra.py
@@ -5,20 +5,17 @@ from graph import Graph
 
 def dijkstra(g: Graph, source: str, destination: str):
     # Declare structures
-    distances : dict = {v: inf for v in g.get_vertex()}
+    distances : dict = {v: float(inf) for v in g.get_vertex()}
     seen : dict = {v: False for v in g.get_vertex()}
     vertices_queue : list = []
 
     # Update source's data
     distances[source] = 0
-    seen[source] = True
     heapq.heappush(vertices_queue, (distances[source], source))
 
     # Iterate over the graph
     while vertices_queue:
-        v : str = heapq.heappop(vertices_queue)
-        v_distance : int = v[0]
-        v_name : str = v[1]
+        v_distance, v_name = heapq.heappop(vertices_queue)
         if v_name == destination:
             break
         seen[v_name] = True
@@ -29,6 +26,8 @@ def dijkstra(g: Graph, source: str, destination: str):
                     heapq.heappush(vertices_queue, (distances[neighbor], neighbor))
 
     # Return the final distance
+    if distances[destination] == float(inf):
+        return "Not reached node"
     return distances[destination]
 
 g = Graph()        
@@ -40,11 +39,14 @@ g.add_edge("uno", "cuatro", 10)
 
 g.add_edge("dos", "tres", 20)
 g.add_edge("dos", "cuatro", 2)
+g.add_edge("dos", "siete", 15)
 
 g.add_edge("tres", "cinco", 4)
 g.add_edge("tres", "seis", 3)
 
+g.add_vertex("ocho")
+
 g.show_graph()
 
 print()
-print(dijkstra(g, "uno", "dos"))
+print(dijkstra(g, "uno", "ocho"))
